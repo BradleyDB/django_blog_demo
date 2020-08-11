def56beebf39de7971833ecb8d91445d0a871614
Bradley <bradley.daybazhaw@gmail.com>
2020-08-11 09:02:07 -0700

    Update wsgi.py

--- blog_proj/mysite/mysite/wsgi.py
+++ blog_proj/mysite/mysite/wsgi.py
@@ -7,10 +7,27 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
+# +++++++++++ DJANGO +++++++++++
+# To use your own django app use code like this:
 import os
+import sys
+from dotenv import load_dotenv
 
+project_folder = os.path.expanduser('~/django_blog_demo/blog_proj/mysite/mysite/')
+load_dotenv(os.path.join(project_folder,'.env'))
+
+#
+## assuming your django settings file is at '/home/bradleydb/mysite/mysite/settings.py'
+## and your manage.py is is at '/home/bradleydb/mysite/manage.py'
+path = '/home/bradleydb/django_blog_demo/blog_proj/mysite/'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.chdir(path)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE","mysite.settings")
+#
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'LVL4.settings'
+#
+## then:
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-
 application = get_wsgi_application()
